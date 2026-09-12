241f782b12bf86a070b50ca9331a30e9eff1d1d0
Abhijeet singh <abhijeetsingh841414Qgmail.com>
2026-09-12 06:36:39 +0530

    Create the present time for user response

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 # Rule based AI python ChatBot
 
-print("==Namaste! Welcome to my personal ChatBot==")
+import datetime
+import time
+
+name = input("Welcome, Enter your name please : ")
+presentHour = datetime.datetime.now().hour
+
+if 5 <= presentHour < 12:
+    print("Good Morning! ",name)
+elif 12 <= presentHour < 17:
+    print("Good Afternoon! ",name)
+elif 17 <= presentHour < 9:
+    print("Good Evening! ",name)
+else:
+    print("Good Night! ",name)
+
+
+print("==== Namaste! Welcome to my personal ChatBot ====")
 print("You can ask me basic question, type 'bye' to exit from the bot ")
 
 responses = {
@@ -13,14 +29,14 @@ responses = {
     "happy" : "Great to hear that",
     "what is python" : "Python is a high-level programming language.",
     "who created python" : "Python was created by Guido ven Rossum",
-    "what is html" : "HTML are created by the dynamic  structure web page.",
+    "what is html" : "HTML are created by the dynamic structure web page.",
     "what is CSS" : "CSS are used to style the webside page.",
     "what is dictionary" : "A dictionary stores data in key-value pairs in python.",
     "what is list" : "A list is a collection that can store multiple values.",
     "what is loop" : "A loop is used to execute a block of code repeatedly",
     "what is function" : "A function is a reusable block of code.",
     "what is github" : "Github us a platform used to host and manage code repositories.",
-    "wour states" : "I am build the ShopEase e-commarse store project by using the HTML, CSS, JavaScript",
+    "our states" : "I am build the ShopEase e-commarse store project by using the HTML, CSS, JavaScript",
     "what is chatbot" : "A chatbot is a program that communicateds with users.",
     "thank you" : "You are welcome",
     "goodbye" : "GoodBye! Have a nice day!",
